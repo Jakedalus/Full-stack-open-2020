624617aebbc597d6b57fa99ddac954a6a0bf8ef3
Jakedalus <jacquedalus@gmail.com>
2020-04-24 17:40:49 -0400

    Finish up lesson D

--- part1/lesson_examples/lesson_c_example_app/src/index.js
+++ part1/lesson_examples/lesson_c_example_app/src/index.js
@@ -82,25 +82,61 @@ import ReactDOM from 'react-dom';
 
 
 ///// MORE COMPLEX STATE
-const App = props => {
-  const [clicks, setClicks] = useState({
-    left: 0, right: 0
-  });
 
-  const handleLeftClick = () => {
-    setClicks({...clicks, left: clicks.left + 1});
-  };
-
-  const handleRightClick = () => {
-    setClicks({...clicks, right: clicks.right + 1});
+const History = props => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    );
   };
 
   return (
     <div>
-      {clicks.left}
-      <button onClick={handleLeftClick}>left</button>
-      {clicks.right}
-      <button onClick={handleRightClick}>right</button>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+};
+
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+);
+
+const App = props => {
+  // const [clicks, setClicks] = useState({
+  //   left: 0, right: 0
+  // });
+  // const handleLeftClick = () => {
+  //   setClicks({...clicks, left: clicks.left + 1});
+  // };
+  // const handleRightClick = () => {
+  //   setClicks({...clicks, right: clicks.right + 1});
+  // };
+
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'));
+    setLeft(left + 1);
+  };
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'));
+    setRight(right + 1);
+  };
+
+  return (
+    <div>
+      {left}
+      <Button handleClick={handleLeftClick} text={'left'} />
+      <Button handleClick={handleRightClick} text={'right'} />
+      {right}
+      <History allClicks={allClicks} />
     </div>
   );
 };
