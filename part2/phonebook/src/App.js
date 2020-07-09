88e4a2a076f196c10ccaf4affde8f3379c13a886
Jacob A. Carpenter <jacquedalus@gmail.com>
2020-07-09 15:06:28 -0400

    Solution to execise 2.6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,19 +4,35 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const addName = (event) => {
+    event.preventDefault();
+    if (newName !== '') {
+      const newPerson = {name: newName};
+      setPersons(persons.concat(newPerson));
+      setNewName('');
+    }
+  }
+
+  const handleOnChange = (event) => {
+    setNewName(event.target.value);
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleOnChange} />
+          <div>debug: {newName}</div>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <div>
+        {persons.map(person => <p key={person.name}>{person.name}</p>)}
+      </div>
     </div>
   );
 };
