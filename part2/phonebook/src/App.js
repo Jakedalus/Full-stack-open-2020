43f57540e24a695a2171f0d44e9a581fa1ed4b41
Jacob A. Carpenter <jacquedalus@gmail.com>
2020-07-09 16:12:54 -0400

    Add solution to 2.9, add search functionality to phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas", number: '555-555-5555' }]);
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '040-123456' },
+  { name: 'Ada Lovelace', number: '39-44-5323523' },
+  { name: 'Dan Abramov', number: '12-43-234345' },
+  { name: 'Mary Poppendieck', number: '39-23-6423122' }]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [search, setSearch] = useState("");
 
   const addName = (event) => {
     // console.log('addName', newName, persons, persons.map(person => person.name).includes(newName));
@@ -22,15 +26,24 @@ const App = () => {
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
-  }
+  };
   
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value);
-  }
+  };
+
+  const handleSearch = (event) => {
+    setSearch(event.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <h3>Search phonebook</h3>
+      <input value={search} onChange={handleSearch} />
+
+      <h3>Add a new entry</h3>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -43,7 +56,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person => <p key={person.name}>{person.name}, {person.number}</p>)}
+        {persons.filter(person => RegExp(search, 'i').test(person.name)).map(person => <p key={person.name}>{person.name}, {person.number}</p>)}
       </div>
     </div>
   );
