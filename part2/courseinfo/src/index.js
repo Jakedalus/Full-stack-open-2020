daaaf9d878290d6327c4f388df655c5cfece1af0
Jacob A. Carpenter <jacquedalus@gmail.com>
2020-07-09 14:23:33 -0400

    Declare Course as a separate module, import Course into index.js

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -1,44 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-const Header = ({ name }) => {
-  return <h1>{name}</h1>;
-};
-
-const Total = ({ course }) => {
-  const sum = course.parts.reduce((sum, part) => sum + part.exercises, 0);
-  return (
-    <p>
-      <strong>Number of exercises {sum}</strong>
-    </p>
-  );
-};
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>
-  );
-};
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map((part) => (
-        <Part part={part} />
-      ))}
-    </div>
-  );
-};
-
-const Course = ({ course }) => (
-  <div>
-    <Header name={course.name} />
-    <Content parts={course.parts} />
-    <Total course={course} />
-  </div>
-);
+import Course from './Course';
 
 const App = () => {
   const courses = [
