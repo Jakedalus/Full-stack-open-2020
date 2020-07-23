69033767778a712a0f097bb48e6b0f52ddfe6e2e
Jacob A. Carpenter <jacquedalus@gmail.com>
2020-07-23 14:31:56 -0400

    Filter countries based on search live, only display if results is 10 or less

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,23 +1,31 @@
 import React, { useState, useEffect } from 'react';
-
-
+import axios from 'axios';
 
 function App() {
+	const [ search, setSearch ] = useState('');
+	const [ countries, setCountries ] = useState([]);
 
-  const [search, setSearch] = useState('');
+	useEffect(() => {
+		axios.get('https://restcountries.eu/rest/v2/all').then(response => {
+			setCountries(response.data);
+		});
+	}, []);
 
-  function handleChange(event) {
-    setSearch(event.target.value);
-  }
+	function handleChange(event) {
+		setSearch(event.target.value);
+	}
 
-  return (
-    <div className="App">
-      <label htmlFor="search">find countries</label>
-      <input onChange={handleChange} type="text" name="search" value={search}/>
-      <ul id="countries">
-      </ul>
-    </div>
-  );
+	const results = countries.filter(country => country.name.toLowerCase().includes(search.toLowerCase()));
+
+	console.log(results);
+
+	return (
+		<div className='App'>
+			<label htmlFor='search'>find countries</label>
+			<input onChange={handleChange} type='text' name='search' value={search} />
+			{results.length <= 10 && <ul id='countries'>{results.map(country => <li>{country.name}</li>)}</ul>}
+		</div>
+	);
 }
 
 export default App;
