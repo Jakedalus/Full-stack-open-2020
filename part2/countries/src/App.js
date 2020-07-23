8c9c20bc5103f259fe87e942df51d5e0c337b090
Jacob A. Carpenter <jacquedalus@gmail.com>
2020-07-23 14:46:27 -0400

    Display country facts when search results narrow down to one

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -6,24 +6,59 @@ function App() {
 	const [ countries, setCountries ] = useState([]);
 
 	useEffect(() => {
-		axios.get('https://restcountries.eu/rest/v2/all').then(response => {
-			setCountries(response.data);
-		});
+		axios
+			.get('https://restcountries.eu/rest/v2/all')
+			.then(response => {
+				setCountries(response.data);
+			});
 	}, []);
 
 	function handleChange(event) {
 		setSearch(event.target.value);
 	}
 
-	const results = countries.filter(country => country.name.toLowerCase().includes(search.toLowerCase()));
+	const results = countries.filter(country =>
+		country.name
+			.toLowerCase()
+			.includes(search.toLowerCase())
+	);
 
 	console.log(results);
 
 	return (
 		<div className='App'>
 			<label htmlFor='search'>find countries</label>
-			<input onChange={handleChange} type='text' name='search' value={search} />
-			{results.length <= 10 && <ul id='countries'>{results.map(country => <li>{country.name}</li>)}</ul>}
+			<input
+				onChange={handleChange}
+				type='text'
+				name='search'
+				value={search}
+			/>
+			{results.length <= 10 &&
+			results.length > 1 && (
+				<ul id='countries'>
+					{results.map(country => <li>{country.name}</li>)}
+				</ul>
+			)}
+
+			{results.length === 1 && (
+				<div>
+					<h2>{results[0].name}</h2>
+					<p>Capital: {results[0].capital}</p>
+					<p>Population: {results[0].population}</p>
+					<p>Languages:</p>
+					<ul>
+						{results[0].languages.map(language => (
+							<li>{language.name}</li>
+						))}
+					</ul>
+					<img
+						src={results[0].flag}
+						alt={`${results[0].name}'s flag`}
+						width='350px'
+					/>
+				</div>
+			)}
 		</div>
 	);
 }
