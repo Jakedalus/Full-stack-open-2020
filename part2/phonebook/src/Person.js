4db6cd69e80de97515473437ac3b570e6234b076
Jacob A. Carpenter <jacquedalus@gmail.com>
2020-08-18 16:01:01 -0400

    Update styling of delete button in phonebook

--- part2/phonebook/src/Person.js
+++ part2/phonebook/src/Person.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 const Person = ({ person, deleteNumber }) => (
 	<div>
-		<p>
+		<p
+			style={{
+				display     : 'inline-block',
+				marginRight : '10px'
+			}}
+		>
 			{person.name}, {person.number}
 		</p>
 		<button onClick={() => deleteNumber(person)}>
