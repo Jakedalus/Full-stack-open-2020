e132b1225c68d2e35b41b12dfbb1faf808c5c517
Jacob A. Carpenter <jacquedalus@gmail.com>
2020-10-15 16:57:04 -0400

    Add middleware, requestLogger and unknownEndpoint

--- part4/app.js
+++ part4/app.js
@@ -3,6 +3,7 @@ const app = express();
 const config = require('./utils/config');
 const cors = require('cors');
 const blogsRouter = require('./controllers/blogs');
+const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
 const mongoose = require('mongoose');
 
@@ -17,7 +18,10 @@ mongoose.connect(config.MONGODB_URI, {
 
 app.use(cors());
 app.use(express.json());
+app.use(middleware.requestLogger);
 
 app.use('/api/blogs/', blogsRouter);
+
+app.use(middleware.unknownEndpoint);
 
 module.exports = app;
