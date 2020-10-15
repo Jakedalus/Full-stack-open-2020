3e6a784101974b0f9cc713cf09f34f98b33cd0db
Jacob A. Carpenter <jacquedalus@gmail.com>
2020-10-15 16:51:22 -0400

    Extract app.js from index.js

--- part4/index.js
+++ part4/index.js
@@ -1,16 +1,10 @@
+const app = require('./app');
 const http = require('http');
-const express = require('express');
-const app = express();
 const config = require('./utils/config');
-const cors = require('cors');
-const blogsRouter = require('./controllers/blogs');
 const logger = require('./utils/logger');
 
-app.use('/api/blogs/', blogsRouter);
+const server = http.createServer(app);
 
-app.use(cors());
-app.use(express.json());
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
 	logger.info(`Server running on port ${config.PORT}`);
 });
