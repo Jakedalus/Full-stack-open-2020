a0925d19498ae01728ce025ff649063284fd9657
Jacob A. Carpenter <jacquedalus@gmail.com>
2020-10-15 18:07:40 -0400

    Add solution to exercise 4.4, helper functions and unit tests step2, totalLikes function

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,3 +1,12 @@
 const dummy = blogs => 1;
 
-module.exports = dummy;
+const totalLikes = list => {
+	console.log('totalLikes, list:', list);
+	return list.length === 0
+		? 0
+		: list.reduce((sum, blog) => {
+				sum + blog.likes;
+			}, 0);
+};
+
+module.exports = { dummy, totalLikes };
