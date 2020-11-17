f149f16080f723fd645741f1e990da40303cde9e
Jacob A. Carpenter <jacquedalus@gmail.com>
2020-11-17 17:26:59 -0500

    Convert blogRoutes handler to async/await versions

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,18 +1,27 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 
-blogsRouter.get('/', (request, response) => {
-	Blog.find({}).then(blogs => {
-		response.json(blogs);
-	});
+blogsRouter.get('/', async (request, response) => {
+	// async version ------
+	const blogs = await Blog.find({});
+
+	response.json(blogs);
+
+	// Blog.find({}).then(blogs => {
+	// 	response.json(blogs);
+	// });
 });
 
 blogsRouter.post('/', (request, response) => {
 	const blog = new Blog(request.body);
 
-	blog.save().then(result => {
-		response.status(201).json(result);
-	});
+	// async version ------
+	const newBlog = blog.save();
+	response.status(201).json(newBlog);
+
+	// blog.save().then(result => {
+	// 	response.status(201).json(result);
+	// });
 });
 
 module.exports = blogsRouter;
