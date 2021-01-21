849c062c659a9adf4daf6532af7f515c1fdc17f7
Jacob A. Carpenter <jacquedalus@gmail.com>
2021-01-21 16:37:38 -0500

    Add solution to 5.7* Blog list frontend, step7, toggle details of blog

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,24 @@
-import React from 'react';
-const Blog = ({ blog }) => (
-	<div>
-		{blog.title} {blog.author}
-	</div>
-);
+import React, { useState } from 'react';
+const Blog = ({ blog }) => {
+	const [ showDetails, setShowDetails ] = useState(false);
+	// console.log('blog', blog);
+	return (
+		<div className='blog-listing'>
+			{blog.title} {blog.author}
+			<button onClick={() => setShowDetails(!showDetails)}>
+				{showDetails ? 'hide' : 'view'}
+			</button>
+			{showDetails && (
+				<div className='blog-listing--details'>
+					<p>{blog.url}</p>
+					<p>
+						{blog.likes} <button>Like</button>
+					</p>
+					<p>created by {blog.user.name}</p>
+				</div>
+			)}
+		</div>
+	);
+};
 
 export default Blog;
