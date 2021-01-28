392eaeb6addb427c2752ec78239e78f82aa73108
Jacob A. Carpenter <jacquedalus@gmail.com>
2021-01-28 18:01:00 -0500

    Add solution to exercise 5.13: Blog list tests, step1, basic render test for Blog component

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -2,3 +2,38 @@ import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { render } from '@testing-library/react';
 import Blog from '../components/Blog';
+
+test('renders Blog content', () => {
+	const blog = {
+		title  : 'Test Title',
+		author : 'Test Author',
+		url    : 'www.test.test',
+		likes  : '66',
+		id     : 'test_id',
+		user   : 'test_user'
+	};
+
+	const editBlog = jest.fn();
+	const deleteBlog = jest.fn();
+
+	const blogComponent = render(
+		<Blog
+			blog={blog}
+			editBlog={editBlog}
+			deleteBlog={deleteBlog}
+		/>
+	);
+
+	expect(blogComponent.container).toHaveTextContent(
+		blog.title
+	);
+	expect(blogComponent.container).toHaveTextContent(
+		blog.author
+	);
+	expect(blogComponent.container).not.toHaveTextContent(
+		blog.url
+	);
+	expect(blogComponent.container).not.toHaveTextContent(
+		blog.likes
+	);
+});
