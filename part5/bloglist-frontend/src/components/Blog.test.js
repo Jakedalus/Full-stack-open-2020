063225961a96ae62001bb1734696261dfe2fd318
Jacob A. Carpenter <jacquedalus@gmail.com>
2021-01-28 18:07:06 -0500

    Add solution to exercise 5.14*: Blog list tests, step2, test showDetails button

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Blog from '../components/Blog';
 
 test('renders Blog content', () => {
@@ -34,6 +34,47 @@ test('renders Blog content', () => {
 		blog.url
 	);
 	expect(blogComponent.container).not.toHaveTextContent(
+		blog.likes
+	);
+});
+
+test('renders extra content after button "View" is clicked', () => {
+	const blog = {
+		title  : 'Test Title',
+		author : 'Test Author',
+		url    : 'www.test.test',
+		likes  : '66',
+		id     : 'test_id',
+		user   : 'test_user'
+	};
+
+	const editBlog = jest.fn();
+	const deleteBlog = jest.fn();
+
+	const currentUser = { id: 'test_user_id' };
+
+	const blogComponent = render(
+		<Blog
+			blog={blog}
+			editBlog={editBlog}
+			deleteBlog={deleteBlog}
+			currentUser={currentUser}
+		/>
+	);
+
+	const showDetailsButton = blogComponent.getByText('view');
+	fireEvent.click(showDetailsButton);
+
+	expect(blogComponent.container).toHaveTextContent(
+		blog.title
+	);
+	expect(blogComponent.container).toHaveTextContent(
+		blog.author
+	);
+	expect(blogComponent.container).toHaveTextContent(
+		blog.url
+	);
+	expect(blogComponent.container).toHaveTextContent(
 		blog.likes
 	);
 });
