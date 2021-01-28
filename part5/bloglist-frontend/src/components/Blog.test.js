a692832ecfad464d6de754e3e821f7282d1d61ac
Jacob A. Carpenter <jacquedalus@gmail.com>
2021-01-28 18:10:50 -0500

    Move tests into describe block, setup beforeEach to render blogComponent for each test

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -3,7 +3,7 @@ import '@testing-library/jest-dom/extend-expect';
 import { render, fireEvent } from '@testing-library/react';
 import Blog from '../components/Blog';
 
-test('renders Blog content', () => {
+describe('Test Blog Component', () => {
 	const blog = {
 		title  : 'Test Title',
 		author : 'Test Author',
@@ -12,69 +12,56 @@ test('renders Blog content', () => {
 		id     : 'test_id',
 		user   : 'test_user'
 	};
+	let blogComponent;
 
-	const editBlog = jest.fn();
-	const deleteBlog = jest.fn();
+	beforeEach(() => {
+		const editBlog = jest.fn();
+		const deleteBlog = jest.fn();
 
-	const blogComponent = render(
-		<Blog
-			blog={blog}
-			editBlog={editBlog}
-			deleteBlog={deleteBlog}
-		/>
-	);
+		const currentUser = { id: 'test_user_id' };
 
-	expect(blogComponent.container).toHaveTextContent(
-		blog.title
-	);
-	expect(blogComponent.container).toHaveTextContent(
-		blog.author
-	);
-	expect(blogComponent.container).not.toHaveTextContent(
-		blog.url
-	);
-	expect(blogComponent.container).not.toHaveTextContent(
-		blog.likes
-	);
-});
+		blogComponent = render(
+			<Blog
+				blog={blog}
+				editBlog={editBlog}
+				deleteBlog={deleteBlog}
+				currentUser={currentUser}
+			/>
+		);
+	});
 
-test('renders details after "view" button is clicked', () => {
-	const blog = {
-		title  : 'Test Title',
-		author : 'Test Author',
-		url    : 'www.test.test',
-		likes  : '66',
-		id     : 'test_id',
-		user   : 'test_user'
-	};
+	test('renders Blog content', () => {
+		expect(blogComponent.container).toHaveTextContent(
+			blog.title
+		);
+		expect(blogComponent.container).toHaveTextContent(
+			blog.author
+		);
+		expect(blogComponent.container).not.toHaveTextContent(
+			blog.url
+		);
+		expect(blogComponent.container).not.toHaveTextContent(
+			blog.likes
+		);
+	});
 
-	const editBlog = jest.fn();
-	const deleteBlog = jest.fn();
+	test('renders details after "view" button is clicked', () => {
+		const showDetailsButton = blogComponent.getByText(
+			'view'
+		);
+		fireEvent.click(showDetailsButton);
 
-	const currentUser = { id: 'test_user_id' };
-
-	const blogComponent = render(
-		<Blog
-			blog={blog}
-			editBlog={editBlog}
-			deleteBlog={deleteBlog}
-			currentUser={currentUser}
-		/>
-	);
-
-	const showDetailsButton = blogComponent.getByText('view');
-	fireEvent.click(showDetailsButton);
-
-	expect(blogComponent.container).toHaveTextContent(
-		blog.title
-	);
-	expect(blogComponent.container).toHaveTextContent(
-		blog.author
-	);
-	expect(blogComponent.container).toHaveTextContent(
-		blog.url
-	);
-	expect(blogComponent.container).toHaveTextContent(
-		blog.likes
-	);
+		expect(blogComponent.container).toHaveTextContent(
+			blog.title
+		);
+		expect(blogComponent.container).toHaveTextContent(
+			blog.author
+		);
+		expect(blogComponent.container).toHaveTextContent(
+			blog.url
+		);
+		expect(blogComponent.container).toHaveTextContent(
+			blog.likes
+		);
+	});
 });
