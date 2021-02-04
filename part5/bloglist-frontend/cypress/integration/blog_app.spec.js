fc401eaec80b6c5a8e39b2e9138bfabe203c5de2
Jacob A. Carpenter <jacquedalus@gmail.com>
2021-02-04 17:51:18 -0500

    Add login form cypress test to bloglist

--- part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,6 +1,14 @@
 describe('Blog App', function() {
-	it('front page can be opened', function() {
+	beforeEach(function() {
 		cy.visit('http://localhost:3000');
+	});
+
+	it('front page can be opened', function() {
 		cy.contains('Blogs');
+	});
+
+	it('login form is shown', function() {
+		cy.contains('Login to Application');
+		cy.get('button').contains('login');
 	});
 });
