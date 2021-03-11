d4bc0538f5cd9410616e0578cfa1a5771c6d776b
Jacob A. Carpenter <jacquedalus@gmail.com>
2021-03-11 18:02:37 -0500

    Add solution to exercise 7.4: anecdotes and hooks step1, use useField custom hook in anecdotes application for CreateNew form

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -3,10 +3,11 @@ import {
 	Switch,
 	Route,
 	Link,
-	useParams,
+	// useParams,
 	useRouteMatch,
 	useHistory
 } from 'react-router-dom';
+import { useField } from './hooks/index';
 
 const Menu = () => {
 	const padding = {
@@ -46,8 +47,8 @@ const AnecdoteList = ({ anecdotes }) => (
 );
 
 const Anecdote = ({ anecdote }) => {
-	const id = useParams().id;
-	console.log('id', id);
+	// const id = useParams().id;
+	// console.log('id', id);
 
 	console.log('anecdote', anecdote);
 	return (
@@ -103,18 +104,22 @@ const Footer = () => (
 );
 
 const CreateNew = props => {
-	const [ content, setContent ] = useState('');
-	const [ author, setAuthor ] = useState('');
-	const [ info, setInfo ] = useState('');
+	// const [ content, setContent ] = useState('');
+	// const [ author, setAuthor ] = useState('');
+	// const [ info, setInfo ] = useState('');
+
+	const content = useField('text');
+	const author = useField('text');
+	const info = useField('text');
 
 	const history = useHistory();
 
 	const handleSubmit = e => {
 		e.preventDefault();
 		props.addNew({
-			content,
-			author,
-			info,
+			content : content.value,
+			author  : author.value,
+			info    : info.value,
 			votes   : 0
 		});
 		history.push('/');
@@ -130,27 +135,15 @@ const CreateNew = props => {
 			<form onSubmit={handleSubmit}>
 				<div>
 					content
-					<input
-						name='content'
-						value={content}
-						onChange={e => setContent(e.target.value)}
-					/>
+					<input {...content} />
 				</div>
 				<div>
 					author
-					<input
-						name='author'
-						value={author}
-						onChange={e => setAuthor(e.target.value)}
-					/>
+					<input {...author} />
 				</div>
 				<div>
 					url for more info
-					<input
-						name='info'
-						value={info}
-						onChange={e => setInfo(e.target.value)}
-					/>
+					<input {...info} />
 				</div>
 				<button>create</button>
 			</form>
@@ -218,8 +211,8 @@ const App = () => {
 				{notification && (
 					<div
 						style={{
-							border: '1px solid black',
-							padding: '5px'
+							border  : '1px solid black',
+							padding : '5px'
 						}}
 					>
 						{notification}
