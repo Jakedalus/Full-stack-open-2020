cf2d6a20de568bf27ddce9699caf44cd03afde2b
Jakedalus <jacquedalus@gmail.com>
2021-06-01 19:00:50 -0400

    Add styling to BlogPage and UserPage

--- part7/bloglist-frontend/src/components/BlogPage.js
+++ part7/bloglist-frontend/src/components/BlogPage.js
@@ -1,6 +1,30 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import styled from 'styled-components';
 import { addComment } from '../reducers/blogReducer';
+
+const StyledDiv = styled.div`
+	display: flex;
+	flex-direction: column;
+
+	padding: 10px;
+	margin-top: 15px;
+	border: 3px solid black;
+	border-radius: 5px;
+	background-color: aqua;
+
+	ul {
+		margin-top: 10px;
+	}
+
+	li {
+		margin-left: 20px;
+	}
+
+	form button {
+		margin-left: 10px;
+	}
+`;
 
 const BlogPage = ({ blog }) => {
 	console.log(`blog`, blog);
@@ -21,7 +45,7 @@ const BlogPage = ({ blog }) => {
 	return (
 		<div>
 			{blog && (
-				<div>
+				<StyledDiv>
 					<h2>{blog.title}</h2>
 					<p>by {blog.author}</p>
 					<a href={`https://${blog.url}`}>{blog.url}</a>
@@ -52,7 +76,7 @@ const BlogPage = ({ blog }) => {
 					{blog.comments.length === 0 && (
 						<div>no comments...</div>
 					)}
-				</div>
+				</StyledDiv>
 			)}
 		</div>
 	);
--- part7/bloglist-frontend/src/components/UserPage.js
+++ part7/bloglist-frontend/src/components/UserPage.js
@@ -1,5 +1,28 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+	display: flex;
+	flex-direction: column;
+
+	padding: 10px;
+	margin-top: 15px;
+	border: 3px solid black;
+	border-radius: 5px;
+	background-color: aqua;
+
+	ul {
+		margin-top: 10px;
+	}
+
+	li {
+		margin-left: 20px;
+	}
+
+	form button {
+		margin-left: 10px;
+	}
+`;
 
 const UserPage = ({ user }) => {
 	// console.log(`users`, users);
@@ -17,13 +40,13 @@ const UserPage = ({ user }) => {
 		<div>
 			<h2>User</h2>
 			{user && (
-				<div>
+				<StyledDiv>
 					<p>{user.name}</p>
 					<p>{user.username}</p>
 					<ul>
 						{user.blogs.map(blog => <li>{blog.title}</li>)}
 					</ul>
-				</div>
+				</StyledDiv>
 			)}
 		</div>
 	);
