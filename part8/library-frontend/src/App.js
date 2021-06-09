e5d9ce6528ea33245917379384c7e10bc5e923e1
Jakedalus <jacquedalus@gmail.com>
2021-06-09 16:20:41 -0400

    Add solution to exercises 8.8: Authors view and 8.9: Books view

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -1,34 +1,52 @@
-
-import React, { useState } from 'react'
-import Authors from './components/Authors'
-import Books from './components/Books'
-import NewBook from './components/NewBook'
+import React, { useState } from 'react';
+import { useQuery } from '@apollo/client';
+import Authors from './components/Authors';
+import Books from './components/Books';
+import NewBook from './components/NewBook';
+import { ALL_AUTHORS, ALL_BOOKS } from './queries/queries';
 
 const App = () => {
-  const [page, setPage] = useState('authors')
+	const [ page, setPage ] = useState('authors');
 
-  return (
-    <div>
-      <div>
-        <button onClick={() => setPage('authors')}>authors</button>
-        <button onClick={() => setPage('books')}>books</button>
-        <button onClick={() => setPage('add')}>add book</button>
-      </div>
+	const authorsResult = useQuery(ALL_AUTHORS, {
+		pollInterval : 2000
+	});
 
-      <Authors
-        show={page === 'authors'}
-      />
+	const booksResult = useQuery(ALL_BOOKS, {
+		pollInterval : 2000
+	});
 
-      <Books
-        show={page === 'books'}
-      />
+	if (authorsResult.loading || booksResult.loading) {
+		return <div>loading...</div>;
+	}
 
-      <NewBook
-        show={page === 'add'}
-      />
+	return (
+		<div>
+			<div>
+				<button onClick={() => setPage('authors')}>
+					authors
+				</button>
+				<button onClick={() => setPage('books')}>
+					books
+				</button>
+				<button onClick={() => setPage('add')}>
+					add book
+				</button>
+			</div>
 
-    </div>
-  )
-}
+			<Authors
+				authors={authorsResult.data.allAuthors}
+				show={page === 'authors'}
+			/>
 
-export default App
+			<Books
+				books={booksResult.data.allBooks}
+				show={page === 'books'}
+			/>
+
+			<NewBook show={page === 'add'} />
+		</div>
+	);
+};
+
+export default App;
