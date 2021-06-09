911cce36306a33f2ef9b4b6fa5ec0b8acf6ca2b4
Jakedalus <jacquedalus@gmail.com>
2021-06-09 15:59:33 -0400

    Add PhoneForm

--- part8/graphql-frontend/src/App.js
+++ part8/graphql-frontend/src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from '@apollo/client';
 import Persons from './Persons';
 import PersonForm from './PersonForm';
+import PhoneForom from './PhoneForm';
 import { ALL_PERSONS } from './queries';
 
 function App() {
+	const [ errorMessage, setErrorMessage ] = useState(null);
+
+	const notify = message => {
+		setErrorMessage(message);
+		setTimeout(() => {
+			setErrorMessage(null);
+		}, 5000);
+	};
+
 	const result = useQuery(ALL_PERSONS, {
 		pollInterval : 2000
 	});
@@ -13,10 +23,21 @@ function App() {
 		return <div>loading...</div>;
 	}
 
+	const Notify = ({ errorMessage }) => {
+		if (!errorMessage) {
+			return null;
+		}
+		return (
+			<div style={{ color: 'red' }}> {errorMessage} </div>
+		);
+	};
+
 	return (
 		<div className='App'>
-			<PersonForm />
+			<Notify errorMessage={errorMessage} />
+			<PersonForm setError={notify} />
 			<Persons persons={result.data.allPersons} />
+			<PhoneForom setError={notify} />
 		</div>
 	);
 }
